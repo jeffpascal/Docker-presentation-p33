a714bbaaf1a02f22718829d15d3c28c860e5cbfb
Pascal Jean-Francois <jeffpascal96@gmail.com>
2024-05-09 02:18:09 +0300

    init

---
+++ example-3-http-app-image-size-optimization/app.js
@@ -0,0 +1,20 @@
+// Load HTTP module
+const http = require("http");
+
+// The hostname and port where the server will run
+const hostname = '0.0.0.0';
+const port = 3000;
+
+// Create HTTP server 
+const server = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Send the response body "Hello, World!"
+  res.end('Hello, World!\n');
+});
+
+// Server listens on hostname and port
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
